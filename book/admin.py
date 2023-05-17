381c84bb3b8ac8edf9995f3205c22ac350232b9a
MaksimB96 <>
2023-05-17 07:18:24 +0000

    Add admin registration

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import SessionBook
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(SessionBook)
+class AdminSession(SummernoteModelAdmin):
+    list_filter = ('confirmed', 'sent_on', 'update_on')
+    summernote_fields = ('client_notes')
